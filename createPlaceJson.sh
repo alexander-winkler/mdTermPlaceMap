cbc6156b32727b404a4aaeb59253d1bf3445dc14
Alexander Winkler <winkler@zib.de>
2026-07-15 08:39:46 +0200

    add actual md data

---
+++ createPlaceJson.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+curl -O "https://term.museum-digital.de/dumps/museum-digital/places_json_de.zip" && unzip places_json_de.zip
+
+echo "Data downloaded and unzipped."
+
+echo "Extract and convert into data.json"
+for f in places_json_de/*.json
+  do jq '{id, name,latitude,longitude,note}' "$f" 2>/dev/null
+done | jq -s '.' > data.json
+
+echo "Generated data.json"
+
+echo "Cleaning up ..."
+rm -r places_json_de
+rm places_json_de.zip
+
+echo "Done!"
+
+ 
